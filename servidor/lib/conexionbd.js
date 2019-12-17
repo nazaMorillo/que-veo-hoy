e1c034cf3f0e55156f2dd39f9c0d02506f520cc2
Nazareno Morillo <nazamorillo1@gmail.com>
2019-12-16 21:43:32 -0300

    agregué packge-lock.json

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 
-  port     : 
-  user     : 
-  password : 
-  database : 
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : '',
+  database : 'que_veo_hoy'
 });
 
 module.exports = connection;
